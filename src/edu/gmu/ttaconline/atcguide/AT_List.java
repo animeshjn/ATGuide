8d76e21fd9cc87b86663d615ba218a129b93d6d3
animeshjn <animesh.jain@hotmail.com>
2017-04-21 16:01:42 -0400

    Assistive tech fragment added EOD 4.21

---
+++ src/edu/gmu/ttaconline/atcguide/AT_List.java
@@ -0,0 +1,141 @@
+package edu.gmu.ttaconline.atcguide;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.w3c.dom.ls.LSInput;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AT_List extends DialogFragment {
+	ATListAdapter listAdapter;
+	ExpandableListView expListView;
+	List<String> listDataHeader;
+	HashMap<String, List<String>> listDataChild;
+	View group;
+	Activity firstTrail;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getDialog().setTitle("Choose AT");
+		View rootView = inflater.inflate(R.layout.activity_at_list, container,
+				false);
+		try {
+			expListView = (ExpandableListView) rootView
+					.findViewById(R.id.lvExp);
+			prepareListData();
+			listAdapter = new ATListAdapter(getActivity()
+					.getApplicationContext(), listDataHeader, listDataChild);
+			// setting list adapter
+			expListView.setAdapter(listAdapter);
+			group = container;
+			expListView
+					.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+						@Override
+						public boolean onChildClick(ExpandableListView parent,
+								View v, int groupPosition, int childPosition,
+								long id) {
+
+							String s = (String) ((TextView) firstTrail
+									.findViewById(R.id.at)).getText();
+							
+							s += ""
+									+ listDataChild.get(
+											listDataHeader.get(groupPosition))
+											.get(childPosition);
+							s += ";";
+							((TextView) firstTrail
+									.findViewById(R.id.at)).setText(s);
+							// ((TextView)group.findViewById(R.id.at)).setText(listDataChild.get(
+							// listDataHeader.get(groupPosition)).get(
+							// childPosition));
+							dismiss();
+							return false;
+						}
+					});
+		} catch (Exception e) {
+			Log.e("DEMO", "Exception " + e);
+		}
+		return rootView;
+	}
+
+	public void setFirstTrail(Activity firstTrail) {
+		this.firstTrail = firstTrail;
+	}
+
+	private void prepareListData() {
+		try {
+			listDataHeader = new ArrayList<String>();
+			listDataChild = new HashMap<String, List<String>>();
+
+			String headers[] = getResources().getStringArray(R.array.atareas);
+			// Adding child data
+			for (String header : headers) {
+				listDataHeader.add(header);
+			}
+
+			// Adding child data
+			List<String> read = new ArrayList<String>();
+			String readAt[] = getResources().getStringArray(
+					R.array.readSolutions);
+
+			for (String solution : readAt) {
+				read.add(solution);
+			}
+
+			List<String> writing = new ArrayList<String>();
+			String writingSol[] = getResources().getStringArray(
+					R.array.writeSolutions);
+			for (String sol : writingSol) {
+				writing.add(sol);
+			}
+
+			List<String> math = new ArrayList<String>();
+			String mathSol[] = getResources().getStringArray(
+					R.array.mathSolutions);
+			for (String sol : mathSol) {
+				math.add(sol);
+			}
+			List<String> listen = new ArrayList<String>();
+			listen.add("No data");
+			/* Complete this for listening */
+
+			listDataChild.put(listDataHeader.get(0), read); // Header, Child
+															// data
+			listDataChild.put(listDataHeader.get(1), writing);
+			listDataChild.put(listDataHeader.get(2), math);
+			listDataChild.put(listDataHeader.get(3), listen);
+			
+		} catch (Exception e) {
+			Log.e("DEMO", "Exception in prepareData() " + e);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- src/edu/gmu/ttaconline/atcguide/FirstTrial.java
+++ src/edu/gmu/ttaconline/atcguide/FirstTrial.java
@@ -1,22 +1,30 @@
 package edu.gmu.ttaconline.atcguide;
 
 import java.util.ArrayList;
+import java.util.Currency;
 
 import com.commonsware.cwac.merge.MergeAdapter;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
 
 /**
  * Activity for first trial of the AT Inputs the additional data for the first
@@ -25,7 +33,7 @@ import android.widget.LinearLayout.LayoutParams;
  * @author animesh jain
  * 
  */
-public class FirstTrial extends Activity {
+public class FirstTrial extends FragmentActivity{
 	/*
 	 * Overview: Type inheriting from activity for the first trial of the AT
 	 * trials
@@ -40,7 +48,9 @@ public class FirstTrial extends Activity {
 	int id = 1001;// default id
 	static int clickedId = 9999;// current clicked id
 	private ArrayList<CharSequence> selectedInstructional; // list of selected
-															// areas
+	android.support.v4.app.FragmentManager fragmentManager=getSupportFragmentManager();
+	Activity activity;
+	// areas
 	/* Methods */
 	// Control start point
 	/**
@@ -55,6 +65,22 @@ public class FirstTrial extends Activity {
 				+ PersistenceBean.getCurrentId(context), context);
 		inflater = getLayoutInflater();
 		placeArea();
+		activity=this;
+		
+		setATListener();
+	}
+	private void setATListener() {
+		ImageButton add= (ImageButton)findViewById(R.id.addat);
+		add.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			AT_List atlist= new AT_List();
+			atlist.setFirstTrail(activity);
+			atlist.show(fragmentManager, "AT List");
+			
+			}
+		});
+		
 	}
 	/**
 	 * Place all areas in the view
@@ -149,7 +175,12 @@ public class FirstTrial extends Activity {
 		getMenuInflater().inflate(R.menu.first_trial, menu);
 		return true;
 	}
-
+	
+	@Override
+	protected void onResume() {
+	Toast.makeText(context,"Resumed",Toast.LENGTH_SHORT).show();
+		super.onResume();
+	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
